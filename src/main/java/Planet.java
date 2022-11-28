1df3d2dee9b36c36935c6f196b0cf26ba963c259
Georose <georgia.hogan123@hotmail.co.uk>
2022-11-28 16:52:04 +0000

    homework

---
+++ src/main/java/Planet.java
@@ -0,0 +1,10 @@
+public class Planet {
+
+    private String name;
+
+    public Planet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {return name;}
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Planet planet = new Planet("Mars");
+        String name = planet.getName();
+        System.out.println(name);
+
+
+    }
+}
